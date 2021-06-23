34ad8168016c833a1e461542cb27f70b83c05851
ColmFL <62991740+ColmFL@users.noreply.github.com>
2021-06-23 13:32:54 +0100

    Bug fix attempt.

--- src/pack/p/GitPluginTestApp.java
+++ src/pack/p/GitPluginTestApp.java
@@ -4,7 +4,7 @@ public class GitPluginTestApp {
 
 	public static void main(String[] args) {
 
-		System.out.println("Hello world!");
+		System.out.println("Hello Git, from branch bug#1234 !!");
 	}
 
 }
